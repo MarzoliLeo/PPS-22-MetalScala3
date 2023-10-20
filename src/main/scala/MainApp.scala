75d78fb9da64e2e3c311b26d5cc1f85ecced7643
Lorenzo Massone <lorenzomassone@gmail.com>
2023-10-20 23:16:49 +0200

    Refactor button click event handling in MainApp.scala
    This commit refactors the main application MainApp.scala, specifically how button clicks are being handled. An ActionEvent import was simplified and a PartialFunction was introduced for two types of buttons: Start and Exit. This made the code cleaner and more maintainable.

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,5 +1,5 @@
 import javafx.application.Application
-import javafx.event.{ActionEvent, EventHandler}
+import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.Button
@@ -10,18 +10,17 @@ class MainApp extends Application:
   @Override def start(primaryStage: Stage): Unit =
     val loader = FXMLLoader(getClass.getResource("main.fxml"))
     val root: GridPane = loader.load[javafx.scene.layout.GridPane]()
+
+    // a PartialFunction to add the handlers to the buttons
+    val onButtonClicked: PartialFunction[Button, Unit] = {
+      case button if button.getText == "Start" => button.setOnAction((_: ActionEvent) => println("Hello World"))
+      case button if button.getText == "Exit" => button.setOnAction((_: ActionEvent) => primaryStage.close())
+    }
+
     root.getChildren
-      .filtered(_.isInstanceOf[Button]).forEach { btn =>
-        val button = btn.asInstanceOf[Button]
-        button.getText match
-          case "Start" =>
-            button.setOnAction((_: ActionEvent) => println("Hello World"))
-          case "Exit" =>
-            button.setOnAction(new EventHandler[ActionEvent] {
-              override def handle(event: ActionEvent): Unit =
-                primaryStage.close()
-            })
-      }
+      .filtered(_.isInstanceOf[Button])
+      .forEach(x => onButtonClicked(x.asInstanceOf[Button]))
+
     primaryStage.setTitle("Hello World")
     primaryStage.setScene(new Scene(root, 640, 480))
     primaryStage.show()
