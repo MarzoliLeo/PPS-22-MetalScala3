df0cc941cff7c282433a565fffc28eb00b441146
Lorenzo Massone <lorenzomassone@gmail.com>
2023-10-21 18:00:23 +0200

    Modularized code regarding dynamic setting of current Scene displayed

---
+++ src/main/scala/App.scala
@@ -0,0 +1,19 @@
+import javafx.application.Application
+import javafx.event.ActionEvent
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.GridPane
+import javafx.stage.Stage
+import view.MainMenu
+
+class App extends Application:
+  @Override def start(primaryStage: Stage): Unit = {
+    val gameTitle = "Metal Scala 3"
+    primaryStage.setTitle(gameTitle)
+    primaryStage.setScene(Scene(MainMenu(primaryStage), 800, 600))
+    primaryStage.show()
+  }
+
+object App:
+  def main(args: Array[String]): Unit = Application.launch(classOf[App], args: _*)
--- src/main/scala/view/MainMenu.scala
+++ src/main/scala/view/MainMenu.scala
@@ -1,29 +1,21 @@
-import javafx.application.Application
+package view
+
 import javafx.event.ActionEvent
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.scene.layout.GridPane
+import javafx.scene.layout.{GridPane, Pane}
 import javafx.stage.Stage
 
-class MainApp extends Application:
-  @Override def start(primaryStage: Stage): Unit =
-    val loader = FXMLLoader(getClass.getResource("main.fxml"))
+object MainMenu:
+  def apply(primaryStage: Stage): Pane =
+    val loader: FXMLLoader = FXMLLoader(getClass.getResource("/main.fxml"))
     val root: GridPane = loader.load[javafx.scene.layout.GridPane]()
-
-    // a PartialFunction to add the handlers to the buttons
     val onButtonClicked: PartialFunction[Button, Unit] = {
       case button if button.getText == "Start" => button.setOnAction((_: ActionEvent) => println("Hello World"))
       case button if button.getText == "Exit" => button.setOnAction((_: ActionEvent) => primaryStage.close())
     }
-
     root.getChildren
       .filtered(_.isInstanceOf[Button])
       .forEach(x => onButtonClicked(x.asInstanceOf[Button]))
-
-    primaryStage.setTitle("Hello World")
-    primaryStage.setScene(new Scene(root, 640, 480))
-    primaryStage.show()
-
-object MainApp:
-  def main(args: Array[String]): Unit = Application.launch(classOf[MainApp], args *)
+    root
