b31b48758ced3ff5d45d14fb87ab6031f5cf42d1
Lorenzo Massone <lorenzomassone@gmail.com>
2023-10-29 22:24:58 +0100

    Added basic command management

--- src/main/scala/view/GameView.scala
+++ src/main/scala/view/GameView.scala
@@ -6,18 +6,20 @@ import javafx.scene.paint.{Color, PhongMaterial}
 import javafx.scene.shape.Box
 import javafx.scene.{Node, Scene}
 import javafx.stage.Stage
-import model.ecs.components._
+import model.ecs.components.*
 import model.ecs.entities.{BoxEntity, Entity, EntityManager}
 import model.ecs.observer.{Observable, Observer}
-import model.input.BasicInputHandler
-import java.util.UUID
 import model.entityManager
+import model.input.BasicInputHandler
+
+import java.util.UUID
 
 trait GameView extends View
 
-private class GameViewImpl(primaryStage: Stage) extends GameView
-  with BasicInputHandler
-  with Observer[Component] {
+private class GameViewImpl(primaryStage: Stage)
+    extends GameView
+    with BasicInputHandler
+    with Observer[Component] {
   val root: FlowPane = FlowPane()
   private var entityIdToView: Map[UUID, Node] = Map()
 
@@ -34,13 +36,21 @@ private class GameViewImpl(primaryStage: Stage) extends GameView
   private val scene: Scene = Scene(root, 800, 600)
   scene.setOnKeyPressed(handleInput)
   primaryStage.setScene(scene)
+  private def removeOldView()(f: => Unit): Unit =
+    Platform.runLater(() => {
+      entityIdToView
+        .foreach((_, view) => root.getChildren.remove(view))
+      f
+    })
 
   override def update(component: Component): Unit =
     removeOldView() {
       component match {
         case position: PositionComponent =>
           boxes
-            .filter(_.getComponent(classOf[PositionComponent]).contains(position))
+            .filter(
+              _.getComponent(classOf[PositionComponent]).contains(position)
+            )
             .foreach(createBoxView)
         case color: ColorComponent =>
           boxes
@@ -62,19 +72,19 @@ private class GameViewImpl(primaryStage: Stage) extends GameView
 
   private def createBoxView(entity: Entity): Unit = {
     val box = Box(100, 100, 100)
-    val position = entity.getComponent(classOf[PositionComponent]).get.asInstanceOf[PositionComponent]
-    val color = entity.getComponent(classOf[ColorComponent]).fold(Color.BLACK)(_.asInstanceOf[ColorComponent].color)
+    val position = entity
+      .getComponent(classOf[PositionComponent])
+      .get
+      .asInstanceOf[PositionComponent]
+    val color = entity
+      .getComponent(classOf[ColorComponent])
+      .fold(Color.BLACK)(_.asInstanceOf[ColorComponent].color)
     box.setTranslateX(position.x)
     box.setTranslateY(position.y)
     box.setMaterial(PhongMaterial(color))
     entityIdToView = entityIdToView + (entity.id -> box)
+    updateView()
   }
-
-  private def removeOldView()(f: => Unit): Unit =
-    Platform.runLater(() => {
-      entityIdToView.foreach((_, view) => root.getChildren.remove(view))
-      f
-    })
 }
 
 object GameView {
