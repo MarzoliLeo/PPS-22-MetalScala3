4f4a94a62b90f64ef4480a1cb40447b3dd5f94da
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-10-31 17:15:12 +0100

    Make getEntityWithComponent variadic

--- src/main/scala/model/ecs/entities/EntityManager.scala
+++ src/main/scala/model/ecs/entities/EntityManager.scala
@@ -3,19 +3,20 @@ package model.ecs.entities
 import model.ecs.components.Component
 import model.ecs.observer.Observable
 
+import scala.collection.immutable.{AbstractSeq, LinearSeq}
+
 trait EntityManager:
   def entities: List[Entity]
   def addEntity(entity: Entity): EntityManager
   def removeEntity(entity: Entity): EntityManager
-  def getEntitiesWithComponent[T <: Component](componentClass: Class[T]): List[Entity]
+  def getEntitiesWithComponent[T <: Component](types: Class[T]*): List[Entity]
   def getEntitiesByClass[T <: Entity](entityClass: Class[T]): List[Entity]
 
 private case class EntityManagerImpl(entities: List[Entity] = List.empty)
     extends EntityManager:
 
-  override def getEntitiesWithComponent[T <: Component](componentClass: Class[T]): List[Entity] = {
-    entities.filter(_.getComponent(componentClass).isDefined)
-  }
+  override def getEntitiesWithComponent[T <: Component](types: Class[T]*): List[Entity] =
+    entities.filter(e => types.forall(t => e.hasComponent(t)))
 
   override def getEntitiesByClass[T <: Entity](entityClass: Class[T]): List[Entity] =
     entities.filter(_.getClass == entityClass)
