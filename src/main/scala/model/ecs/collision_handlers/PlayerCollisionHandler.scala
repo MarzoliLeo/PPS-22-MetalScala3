f7920737ce33555c0b56e9f0dcf8399d362c4af7
Lorenzo Massone <lorenzomassone@gmail.com>
2023-11-29 11:17:02 +0100

    Refactored PlayerCollisionHandler to be simpler and more readable

--- src/main/scala/model/ecs/collision_handlers/PlayerCollisionHandler.scala
+++ src/main/scala/model/ecs/collision_handlers/PlayerCollisionHandler.scala
@@ -1,13 +1,9 @@
 package model.ecs.collision_handlers
-import model.HORIZONTAL_COLLISION_SIZE
-import model.VERTICAL_COLLISION_SIZE
-import model.ecs.components.JumpingComponent
-import model.ecs.components.PositionComponent
-import model.ecs.components.VelocityComponent
+import model.{HORIZONTAL_COLLISION_SIZE, VERTICAL_COLLISION_SIZE}
+import model.ecs.components.{JumpingComponent, PositionComponent, VelocityComponent}
 import model.ecs.entities.Entity
 import model.ecs.systems.CollisionChecker
-import model.ecs.systems.CollisionChecker.boundaryCheck
-import model.ecs.systems.CollisionChecker.getCollidingEntity
+import model.ecs.systems.CollisionChecker.{boundaryCheck, getCollidingEntity}
 
 trait PlayerCollisionHandler extends CollisionHandler:
   self: Entity =>
@@ -27,19 +23,39 @@ trait PlayerCollisionHandler extends CollisionHandler:
       proposedPosition: PositionComponent,
       velocity: VelocityComponent
   ): JumpingComponent =
-    if ((currentPosition.y + VERTICAL_COLLISION_SIZE >= model.GUIHEIGHT && velocity.y >= 0)
-      || getCollidingEntity(this, PositionComponent(currentPosition.x, proposedPosition.y)).isDefined)
+    if (
+      (currentPosition.y + VERTICAL_COLLISION_SIZE >= model.GUIHEIGHT && velocity.y >= 0)
+      || getCollidingEntity(
+        this,
+        PositionComponent(currentPosition.x, proposedPosition.y)
+      ).isDefined
+    )
       JumpingComponent(false)
     else
-      getComponent[JumpingComponent].getOrElse(throw new Exception("No JumpingComponent found"))
+      getComponent[JumpingComponent].getOrElse(
+        throw new Exception("No JumpingComponent found")
+      )
+
+  /**
+   * Handles the collision of the PlayerEntity with the proposed position.
+   * @param proposedPosition
+   *   The proposed position before the collision is handled.
+   *  @return
+   *   An optional PositionComponent representing the new position after
+   *   handling the collision. Returns None if no position update is necessary.
+   */
   override def handleCollision(
       proposedPosition: PositionComponent
   ): Option[PositionComponent] =
+    // TODO: check if the collision is with a weapon:
+    //  if so, delete the weapon entity and change the type of bullets shot
+
     for {
       currentPosition <- getComponent[PositionComponent]
       velocity <- getComponent[VelocityComponent]
     } yield {
-      val updatedJumpingComponent = updateJumpingComponent(currentPosition, proposedPosition, velocity)
+      val updatedJumpingComponent =
+        updateJumpingComponent(currentPosition, proposedPosition, velocity)
       replaceComponent(updatedJumpingComponent)
 
       PositionComponent(
